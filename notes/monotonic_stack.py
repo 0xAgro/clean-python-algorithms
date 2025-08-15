bc9d3be485e5de44d1894234d7927138fe7bc496
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-15 00:56:44 -0400

    Update monotonic_stack.py

--- notes/monotonic_stack.py
+++ notes/monotonic_stack.py
@@ -1,3 +1,13 @@
+# ==============================
+# 📹 0xAgro Code Tutorial Playlist
+# ==============================
+# This code is part of a tutorial series.
+# For detailed explanations and demonstrations, check out the playlist:
+# 
+# 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVeWoPtKptbuBXNDccVAGOdU>
+# ==============================
+
+
 def NGE(self, A):
   A = A[:]
   stack = []
