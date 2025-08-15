206f05ec9ca1325b61f3cf9b365b7c67c77bda72
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-15 00:43:05 -0400

    Update monotonic_stack.py

--- notes/monotonic_stack.py
+++ notes/monotonic_stack.py
@@ -1,1 +1,14 @@
+def NGE(self, A):
+  stack = []
+  res = []
 
+  while A:
+    a = A.pop()
+            
+    while stack and a > stack[-1]:
+      stack.pop()
+
+    res.append(-1 if not stack else stack[-1])
+    stack.append(a)
+
+  return res[::-1]
