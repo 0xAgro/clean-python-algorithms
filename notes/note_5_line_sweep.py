8046ef7a87c1159406a2130916e31de672068cb1
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-18 23:38:58 -0400

    Update note_5_line_sweep.py

--- notes/note_5_line_sweep.py
+++ notes/note_5_line_sweep.py
@@ -1,1 +1,9 @@
-
+# ==============================
+# 📹 0xAgro Code Tutorial Playlist
+# ==============================
+# This code is part of a tutorial series.
+# For detailed explanations and demonstrations, check out the playlist:
+# 
+# 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVeWoPtKptbuBXNDccVAGOdU>
+# 🎥 Watch Note #5 Line Sweep here: <https://www.youtube.com/shorts/8V5wkJgs9eU>
+# ==============================
