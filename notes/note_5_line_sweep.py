69b53b3f92bd6a7aa7f3b9a1404b1c5421856c80
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-19 00:05:24 -0400

    Update note_5_line_sweep.py

--- notes/note_5_line_sweep.py
+++ notes/note_5_line_sweep.py
@@ -7,3 +7,36 @@
 # 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVeWoPtKptbuBXNDccVAGOdU>
 # 🎥 Watch Note #5 Line Sweep here: <https://www.youtube.com/shorts/8V5wkJgs9eU>
 # ==============================
+
+# Version shown in the video (expensive)
+# I -> [[start, end]]
+def line_sweep(self, I):
+  A = [0]*(max([e for _, e in I]) + 1) # Setting array size to max end value
+
+  # Add 1 to the start value, subtract 1 from the end
+  for s, e in I:
+    A[s] = A[s] + 1 
+    A[e] = A[e + 1] - 1
+
+  # Running sum
+  for idx in range(1, len(A)):
+    A[idx] += A[idx - 1]
+
+  return A #returning the interval overlap array
+
+# I -> [[start, end]]
+# SortedDict version (less expensive)
+def line_sweep_sd(self, I):
+  d = SortedDict()
+
+  for s, e in I:
+    d[s] = d[s] + 1 if s in d else 1
+    d[e] = d[e + 1] - 1 if e + 1 in d else -1
+
+  res, cnt = 0, 0 
+
+  for val in self.sd.values():
+    cnt += val
+    # interact with the interval sizes here by setting res
+
+  return res # Replace with the value you want to return
