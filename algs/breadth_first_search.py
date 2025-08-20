594b9d5258fd67f58ab9080d196bd3347b4b610f
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-20 02:48:11 -0400

    Update breadth_first_search.py

--- algs/breadth_first_search.py
+++ algs/breadth_first_search.py
@@ -7,3 +7,19 @@
 # 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVcO8NJ4BnEmNwy-pAvuckvg>
 # 🎥 Watch Breadth First Search here: <>
 # ==============================
+
+# g -> some graph
+# In this case g is of form: [[1,4], [1, 2], ...] 
+# (each index indicate what other indecies they are connected to)
+def bfs(self, g):
+  q = deque([0]) # Initialize double ended queue with root / desired start
+
+  while q:
+    node = q.popleft() # Pop node and other items if needed for logic
+
+    print(node) # Replace with some logic
+
+    for adj in g[node]: # Check all adjacent nodes
+      q.append(adj)
+
+  return -1 # Return desired value
