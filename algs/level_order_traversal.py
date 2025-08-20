6fe18583bd2057d59beff0bc565118e30ef89179
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-20 02:51:55 -0400

    Update level_order_traversal.py

--- algs/level_order_traversal.py
+++ algs/level_order_traversal.py
@@ -7,3 +7,20 @@
 # 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVcO8NJ4BnEmNwy-pAvuckvg>
 # 🎥 Watch Level Order Traversal here: <>
 # ==============================
+
+def lot(self, root)
+
+  q = deque([root])
+
+  while q:
+    
+    for idx in range(len(q)):
+      node = q.popleft()
+
+      if node.left:
+        q.append(node.left)
+
+      if node.right:
+        q.append(node.right)
+      
+  return -1 
