fc5e2e33f9dd3cd758555a59062aba0895b3d0ba
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-20 02:39:21 -0400

    Update depth_first_search.py

--- algs/depth_first_search.py
+++ algs/depth_first_search.py
@@ -1,1 +1,9 @@
-
+# ==============================
+# 📹 0xAgro Code Tutorial Playlist
+# ==============================
+# This code is part of a tutorial series.
+# For detailed explanations and demonstrations, check out the playlist:
+# 
+# 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVcO8NJ4BnEmNwy-pAvuckvg>
+# 🎥 Watch Depth First Search here: <>
+# ==============================
