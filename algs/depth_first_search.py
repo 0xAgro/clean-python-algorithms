38ffed897ecf734a6cfde73f41da60bd91f9b134
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-08-20 02:48:25 -0400

    Update depth_first_search.py

--- algs/depth_first_search.py
+++ algs/depth_first_search.py
@@ -7,3 +7,20 @@
 # 🎥 Watch the full playlist here: <https://www.youtube.com/playlist?list=PLpCYhic-IxVcO8NJ4BnEmNwy-pAvuckvg>
 # 🎥 Watch Depth First Search here: <>
 # ==============================
+
+# g -> some graph
+# In this case g is of form: [[1,4], [1, 2], ...] 
+# (each index indicate what other indecies they are connected to)
+def dfs(self, g):
+  stack = [0] # Initialize stack with root / desired start
+
+  while stack:
+    node = stack.pop() # Pop node and other items if needed for logic
+
+    print(node) # Replace with some logic
+
+    for adj in g[node]: # Check all adjacent nodes
+      stack.append(adj)
+
+  return -1 # Return desired value
+  
