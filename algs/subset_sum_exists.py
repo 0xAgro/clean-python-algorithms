4109b5637d3aa302f0ec143134d6c07195ab6e97
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-09-18 05:03:58 -0400

    Update subset_sum_exists.py

--- algs/subset_sum_exists.py
+++ algs/subset_sum_exists.py
@@ -4,7 +4,8 @@ class Solution:
             return False
 
         dp = [[0]*(len(A) + 1) for _ in range(sum(A) + 1)]
-        dp[0][0] = 1
+        for j in range(len(dp[0])):
+            dp[0][j] = 1
         
         for j in range(1, len(dp[0])):
             for i in range(1, len(dp)):
