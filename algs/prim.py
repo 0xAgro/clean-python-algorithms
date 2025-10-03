5009679e6cab51b291642e4b71ca52e66f84ad3a
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-10-03 06:05:26 -0400

    Update prim.py

--- algs/prim.py
+++ algs/prim.py
@@ -1,1 +1,26 @@
+class Solution:
+    def minCostConnectPoints(self, P: List[List[int]]) -> int:
+        n = len(P)
+        seen = set()
+        g = {}
 
+        h = [[0, P[0]]]
+        heapify(h)
+
+        res = 0
+
+        while len(seen) < n:
+
+            cost, (x, y) = heappop(h)
+
+            if (x, y) in seen:
+                continue
+            else:
+                seen.add((x, y))
+                res += cost
+
+            for (u, v) in P:
+                c = abs(x - u) + abs(y - v)
+                heappush(h, (c, (u, v)))
+
+        return res
