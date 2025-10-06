5b2036d31a1df7a4c6319852b7b2537f72c71893
0xAgro <118331306+0xAgro@users.noreply.github.com>
2025-10-06 06:34:04 -0400

    Update bitmask.py

--- algs/bitmask.py
+++ algs/bitmask.py
@@ -1,1 +1,15 @@
+class Bitmask:
+    def __init__(self):
+        self.mask = 0
 
+    def add(self, i):
+        self.mask |= (1 << i)
+
+    def flip(self, i):
+        self.mask ^= (1 << i)
+
+    def delete(self, i):
+        self.mask &= ~(1 << i)
+
+    def read(self, i):
+        return (self.mask >> i) & 1
